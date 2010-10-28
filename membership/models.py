9962630d449a95fd37e368ea9b840b52ef153675
Eli Bixby <ebixby@oberlin.edu>
2010-10-27 23:49:27 -0400

    added the waitlist class, did some preliminary sketching of functions dealing with the waitlist

--- membership/models.py
+++ membership/models.py
@@ -1,19 +1,21 @@
 from django.db import models
+import random
 
 
 class Applicant(User):"this class extends Django's built-in user model and is an abstract class"
     preferences = []
     lottery_number = models.IntegerField()
-    def set_preference(coop,rank):"adds a preference to the preference list, replaces preference if it already exists"
+    def set_preference(coop,rank):
+        "adds a preference to the preference list, replaces preference if it already exists"
         remove_preference(coop)
         preferences.insert(rank,coop)
     
     def remove_preference(coop):
         if(preferences.in(coop)):
-            i = preferences.index(coop)
-            preferences = preferences.ext[:i].extend(preferences[i+1:])
+            preferences.remove(coop)
         
-    def get_rank(coop):"returns the rank of the element in the preference list"
+    def get_rank(coop):
+        "returns the rank of the element in the preference list"
         if(preferences.in(coop)):
             return preferences.index(coop)
         else:
@@ -50,4 +52,28 @@ class Job(models.Model):
     jumps_housing = models.BooleanField()
     jumps_dining = models.BooleanField()
 "       
+
+class Waitlist(models.Model):
+    loterry_date = models.DateField()
+    valid_until = models.DateField()
+    applicants = ManyToManyField(Applicants)
+    
+    def populate():
+        numbers = range(self.applicants_set.len+1)
+        numbers.remove(0)
+        for appl in self.applicants_set.all():
+            i = random.randint(1,numbers.len)
+            appl.lottery_number = i
+            numbers.remove(i)
+            
+    def add:
+        if(lottery_date is before now):
+            add person to end of waitlist
+        else:
+            do nothing (person should be added to applicants.) 
+        
+
+    def remove(user):
+        if(applicants.in(user)):
+            applicants.in(user):
     
