cbafe53699b9a70bb911f2ff2b127fb4a0a53c8e
Eli Bixby <eli@eli-desktop.(none)>
2011-01-15 18:12:53 -0500

    Corrected numerous errors, added unicode definitions for models, added ordering in relations

--- membership/models.py
+++ membership/models.py
@@ -1,88 +1,101 @@
 from django.db import models
 import random
 
-
+class Applicant(models.Model):
+	user = models.OneToOneField(User_Profile)
+	is_member = models.BooleanField()
+	def __unicode__(self):
+		return self.user
+	class Meta:
+		ordering = ['user']
+	
+    
 class Semester(models.Model):
-    start_date = models.DateField()
-    end_date = models.DateField()
-    is_current = models.BooleanField()
-    lottery_date = models.DateTimeField()
+	start_date = models.DateField()
+	end_date = models.DateField()
+	is_current = models.BooleanField()
+	lottery_date = models.DateTimeField()
+	def __unicode__(self):
+		return self.start_date
+	class Meta:
+		ordering = ['start_date']
 
-class DiningMember(models.Model):
-    coop = models.ForeignKey(DiningCoop)
-    user = models.ForeignKey(User)
-    semester = models.ForeignKey(Semester)
+class Dining_Member(models.Model):
+	coop = models.ForeignKey(Dining_Coop)
+	applicant = models.ForeignKey(Applicant)
+	semester = models.ForeignKey(Semester)
+	def __unicode__(self):
+		return self.applicant
+	class Meta:
+		ordering = ['applicant']
 
-class HousingMember(models.Model):
-    coop = models.ForeignKey(HousingCoop)
-    user = models.ForeignKey(User)
-    semester = models.ForeignKey(Semester)
-    positions = models.ManyToManyField(Jobs.Position)
-		
-"option one for coop system"
+class Housing_Member(models.Model):
+	coop = models.ForeignKey(Housing_Coop)
+	applicant = models.ForeignKey(Applicant)
+	semester = models.ForeignKey(Semester)
+	def __unicode__(self):
+		return self.applicant
+	class Meta:
+		ordering = ['applicant']
+
 class Coop(models.Model):
-    name = models.CharField(max_length=20)
-    street_address = models.CharField(max_length=100)
-    description = models.TextField()
-    picture = models.ImageField(upload_to=self.name/filename)
-
-
-class DiningCoop(models.Model):
-    coop = models.OneToOneField(Coop, related_name='dining')
-    members = models.ManyToManyField(User, through=DiningMember)
-    capacity = models.PositiveIntegerField()
-
-
-class HousingCoop(models.Model):
-    coop = models.OneToOneField(Coop, related_name='housing')
-    members = models.ManyToManyField(User, through=HousingMember)
-    capacity = models.PositiveIntegerField()
-
-
-"option two for coop system" 
-class Coop(models.Model):
-    name = models.CharField(max_length=20)
-    street_address = models.CharField(max_length=100)
-    capacity = models.PositiveIntegerField()
-    description = models.TextField()
-    picture = models.ImageField(upload_to=self.name/filename) 
-    class Meta:
-        abstract = True
+	name = models.CharField(max_length=20)
+	street_address = models.CharField(max_length=100)
+	capacity = models.PositiveIntegerField()
+	description = models.TextField()
+	picture = models.ImageField(upload_to=self.name/filename)
+	def __unicode__(self):
+		return self.name
+	class Meta:
+		abstract = True
+		ordering = ['name']
 
 class Dining_Coop(Coop):
-    members = ManyToManyField(User, through=DiningMember)
+	members = ManyToManyField(Applicant, through=DiningMember)
 	
 class Housing_Coop(Coop):
-    members = ManyToManyField(User, through=HousingMember)
+	members = ManyToManyField(Applicant, through=HousingMember)
 
 class Application(models.Model):
-    waitlist = models.ForeignKey(Waitlist)
-    applicant = models.ForeignKey(User)
-    
-class Preference(models.Model):
-    application = models.ForeignKey(Application)
-    coop = models.ForeignKey(Coop)
-    order = models.PositiveSmallIntegerField()
-			
-class Waitlist(models.Model):
-    semester = models.OneToOneField(Semester)
-    applicants = ManyToManyField(User, through=Application)
-    def populate():
-        numbers = range(self.applicants_set.len+1)
-        numbers.remove(0)
-        for appl in self.applicants_set.all():
-            i = random.randint(1,numbers.len)
-            appl.lottery_number = i
-            numbers.remove(i)
-            
-    def add:
-        if(lottery_date is before now):
-            add person to end of waitlist
-        else:
-            do nothing (person should be added to applicants.) 
-        
+	waitlist = models.ForeignKey(Waitlist)
+	applicant = models.ForeignKey(Applicant)
+	lottery_number = models.PositiveIntegerField()
+	def __unicode__(self):
+		return self.applicant
+	class Meta:
+		order_with_respect_to = 'waitlist'
+		ordering = ['lottery_number']
 
-    def remove(user):
-        if(applicants.in(user)):
-            applicants.in(user):
-    
+class Preference(models.Model):
+	application = models.ForeignKey(Application)
+	coop = models.ForeignKey(Coop)
+	order = models.PositiveSmallIntegerField()
+	def __unicode__(self):
+		return self.coop
+	class Meta:
+		unique_together = ((application, order),(application, coop))
+		order_with_respect_to = 'application'
+		ordering = ['order']
+
+class Waitlist(models.Model):
+	semester = models.OneToOneField(Semester)
+	applicants = ManyToManyField(Applicant, through=Application)
+	size = models.PositiveIntegerField()
+	def do_lottery():
+		numbers = range(self.size)
+		for appl in self.applicants_set.all():
+			i = random.randint(0,self.size)
+			appl.lottery_number = i
+			numbers.remove(i)
+	def add(Applicant):
+		if(self.semester.lottery_date. is before now):
+		   add person to end of waitlist
+		else:
+		   do nothing (person should be added to applicants.)
+	def remove(user):
+		if(applicants.in(user)):
+			applicants.in(user)
+	def __unicode__(self):
+		return self.semester
+	class Meta:
+		ordering = ['semester']
--- settings.py
+++ settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'osca.db',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '3edcvbnhy654',                  # Not used with sqlite3.
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '~/Documents/databases/oscadb',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -90,7 +90,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
